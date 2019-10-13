b56ce39b8bbdfbae0abb6700a5f2690686784303
Efe <knowledgeagare157@gmail.com>
2019-10-13 15:01:06 +0100

    create book type and enums

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -1,4 +1,9 @@
 module Types
-
-  
+  class BookType < Base::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :author, Types::AuthorType, null: false
+    field :publication_date, Integer, null: false
+    field :genre, Enums::Genre, null: false
+  end
 end
