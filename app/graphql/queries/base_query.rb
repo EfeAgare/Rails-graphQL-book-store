bc5f102eba958b87e8bf047d196e9aeed794d3e8
Efe <knowledgeagare157@gmail.com>
2019-10-14 18:43:04 +0100

    write a book query on a seperate file

---
+++ app/graphql/queries/base_query.rb
@@ -0,0 +1,6 @@
+module Queries
+  class BaseQuery < GraphQL::Schema::Resolver
+    
+  end
+  
+end
---
+++ app/graphql/queries/book.rb
@@ -0,0 +1,14 @@
+module Queries
+    class Book < Queries::BaseQuery
+      description "find a book by ID"
+
+      argument :id, ID, required: true
+
+
+      type Types::BookType, null: false
+
+      def resolve(id:)
+        ::Book.find(id)
+      end
+    end  
+end
