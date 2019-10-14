447a4417448a98d85bfecc233309a1ae6536bc0f
Efe <knowledgeagare157@gmail.com>
2019-10-14 15:36:50 +0100

    new working authors query

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,4 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_book, mutation: Mutations::Books::CreateBook
-    field :update_book, mutation: Mutations::Books::UpdateBook
-    field :destroy_book, mutation: Mutations::Books::DestroyBook
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,28 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    ## query
+    field :authors, [Types::AuthorType], null: false do
+      description "fetch all authors"
+    end
+      
+    field :author, Types::AuthorType, null: false do
+      description "fetch an author with the id"
+
+      argument :id, ID, required: true
+    end
+
+
+
+
+
+    ## resolvers
+    def authors
+     Author.all
+    end
+
+    def author(id:)
+      Author.find_by(id: id)
     end
   end
 end
